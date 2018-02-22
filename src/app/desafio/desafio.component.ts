385780d50ddba78dae696ba6b8540d0806675392
witsdigital <34650770+witsdigital@users.noreply.github.com>
2018-02-22 10:54:48 -0300

    update desafio

--- src/app/desafio/desafio.component.ts
+++ src/app/desafio/desafio.component.ts
@@ -10,16 +10,27 @@ import { PainelService } from '../services/painel.service';
 export class DesafioComponent implements OnInit {
 
   time: any =[];
+
   times:any = [];
   userData: any = {};
+  tempo = 0;
+
+
+  jogo: any;
+
 
   constructor(public service: PainelService, private modal: NgbModal) {
     this.userData = JSON.parse(localStorage.getItem('userData'));
+    
     this.getTime();
 
-    setInterval(() => { 
+    setTimeout(() => {     
       this.getTimes();
      }, 1000);
+
+     setInterval(() => {     
+      this.getTimes();
+     }, 60000);
 
    }
 
@@ -29,8 +40,6 @@ export class DesafioComponent implements OnInit {
   getTime(){
     this.service.getTime(this.userData[0].id).then((data)=>{
         this.time = data;
-        console.log(this.time);
-
     },(err)=>{
 
     });
@@ -38,14 +47,113 @@ export class DesafioComponent implements OnInit {
   
 }
 
+desafiar(item) {
+  this.jogo = {
+    time: this.time[0].nome,
+    adversario: item.nome,
+    gols_1: 0,
+    gols_2: 0,
+    Equipe_v: ''
+  };
+
+  var tempo = setInterval(() => { 
+    this.tempo++;
+    if (this.tempo>=60){
+      clearInterval(tempo);
+     }
+   }, 1000);
+
+   var diferença1 = (this.time[0].atk_maximo + Math.floor((Math.random() * 1000) + 1)) - (item.defesa_maxima + Math.floor((Math.random() * 1000) + 1));
+   var diferença2 = (item.atk_maximo + Math.floor((Math.random() * 1000) + 1)) - (this.time[0].defesa_maxima + Math.floor((Math.random() * 1000) + 1));
+   var resistencia1 = this.time[0].resistencia_maxima;
+   var resistencia2 = item.resistencia_maxima;
+   var gol1 = 0;
+   var gol2 = 0;
+
+   if (diferença1 <= 0){
+    var x = Math.floor((Math.random() * 200) + 1);
+    diferença1 = diferença2 + x;
+   }
+
+   if (diferença2 <= 0){
+    var x = Math.floor((Math.random() * 200) + 1);
+    diferença2 = diferença2 + x;
+   }
+
+   if (diferença1 <= -300){
+    var x = Math.floor((Math.random() * 500) + 1);
+    diferença1 = diferença2 + x;
+   }
+
+   if (diferença2 <= -300){
+    var x = Math.floor((Math.random() * 500) + 1);
+    diferença2 = diferença2 + x;
+   }
+
+   if (diferença1 <= -500){
+    var x = Math.floor((Math.random() * 1000) + 1);
+    diferença1 = diferença2 + x;
+   }
+
+   if (diferença2 <= -500){
+    var x = Math.floor((Math.random() * 1000) + 1);
+    diferença2 = diferença2 + x;
+   }
+
+   if (diferença1 <= -1000){
+    var x = Math.floor((Math.random() * 1500) + 1);
+    diferença1 = diferença2 + x;
+   }
+
+   if (diferença2 <= -1000){
+    var x = Math.floor((Math.random() * 1500) + 1);
+    diferença2 = diferença2 + x;
+   }
+
+
+   var jogo = setInterval(() => {   
+    resistencia1 =  resistencia1 - diferença2;
+    resistencia2 =  resistencia2 - diferença1;
+    console.log(resistencia1);
+    console.log(resistencia2);
+    if (resistencia1<=0){
+      gol2++;
+      this.jogo = {
+        time: this.time[0].nome,
+        adversario: item.nome,
+        gols_1: gol1,
+        gols_2: gol2,
+        Equipe_v: ''
+      };
+    }
+
+    if (resistencia2<=0){
+      gol1++;
+      this.jogo = {
+        time: this.time[0].nome,
+        adversario: item.nome,
+        gols_1: gol1,
+        gols_2: gol2,
+        Equipe_v: ''
+      };
+    }
+
+    if (this.tempo==60){
+      clearInterval(jogo);
+     }
+   }, 6000);
+  
+
+}
+
 
 getTimes(){
   this.service.getTimes(this.time[0].id).then((data)=>{
       this.times = data;
-      console.log(this.time);
-
   },(err)=>{
 
   });
+
+}
 
 }
